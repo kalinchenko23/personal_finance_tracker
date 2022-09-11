2964c821370fca82fd9e395c092b49a5d050f548
Maksym Kalinchenko <kalinchenko.max@gmail.com>
2022-09-10 20:43:29 -0400

    Created "backend" folder

--- backend/fastapi/api_service.py
+++ backend/fastapi/api_service.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.insert(0, '/Users/maximkalinchenko/Desktop/personal_finance_tracker/backend/plaid_service')
-sys.path.insert(0, '/Users/maximkalinchenko/Desktop/personal_finance_tracker/backend/database')
+sys.path.insert(1, '/Users/maximkalinchenko/Desktop/personal_finance_tracker/backend/database')
+sys.path.insert(2, '/Users/maximkalinchenko/Desktop/personal_finance_tracker/backend/database/fastapi_sql')
 from fastapi import FastAPI, Query
 import uvicorn
 from plaid_dashboard import plaid_service
 from pydantic_models import Accounts_pydantic
 from pydantic import BaseModel
-from fastapi_sql import User
+from fastapi_sql_service import User
 data=plaid_service.get_acounts_info
 app = FastAPI()
 
