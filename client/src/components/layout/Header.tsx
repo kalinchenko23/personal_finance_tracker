4c89e9adae5bbac78139f67808b5e58bdb634f9f
Ruslan Useinov <r.useinov23@gmail.com>
2022-09-13 18:41:36 -0400

    Theme Context implemented

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,22 +1,44 @@
 import * as React from 'react';
 import LoginForm from '@/components/LoginForm';
+import clsx from 'clsx';
+import {FaMoon, FaSun} from "react-icons/fa";
 
 import UnstyledLink from '@/components/links/UnstyledLink';
+import PrimaryLink from '@/components/links/PrimaryLink'
 import Logo from '@/components/Logo';
 import Button from '../buttons/Button';
+import { ThemeContext } from '@/context/ThemeProvider';
 
-const DesktopNav = ({setOpenLogin}:{setOpenLogin:React.Dispatch<React.SetStateAction<boolean>>})=>{
+
+const DesktopNav:React.FC<{
+  mode:'light'|'dark'
+  toggleMode:()=>void
+  setOpenLogin:React.Dispatch<React.SetStateAction<boolean>>
+}> = ({mode, toggleMode, setOpenLogin})=>{
   return (
-    <ul className='hidden items-center justify-between space-x-4  lg:flex'>
+    <ul className='hidden items-center justify-between space-x-4  lg:flex '>
       {links.map(({ href, label }) => (
         <li key={`${href}${label}`}>
-          <UnstyledLink href={href} className='hover:text-gray-600'>
+          <UnstyledLink href={href} className=''>
             {label}
           </UnstyledLink>
         </li>
       ))}
         <li key={`login`}>
-          <Button variant='light' onClick={()=>setOpenLogin(true)}>Login</Button>
+          <Button 
+            className='rounded-lg'
+            variant={mode === 'dark' ? 'light' : 'dark'}
+            onClick={()=>setOpenLogin(true)}
+          >Login
+          </Button>
+        </li>
+        <li key={`login`}>
+          <Button 
+              variant={mode === 'dark' ? 'light' : 'dark'}
+              className='rounded-lg text-2xl' 
+              onClick={toggleMode}
+            >{mode === 'light' ? <FaMoon/> : <FaSun/>}
+            </Button>
         </li>
     </ul>
   )
@@ -36,9 +58,9 @@ const MobileNavDropDown = ()=>{
       <ul className="absolute hidden text-gray-700 pt-1 group-hover:block">
         {links.map(({ href, label }) => (
             <li key={`${href}${label}`}>
-              <UnstyledLink href={href} className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 flex flex-col justify-start whitespace-no-wrap '>
+              <PrimaryLink href={href} className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 flex flex-col justify-start whitespace-no-wrap '>
                 {label}
-              </UnstyledLink>
+              </PrimaryLink>
             </li>
           ))}
       </ul>
@@ -46,31 +68,26 @@ const MobileNavDropDown = ()=>{
   )
 };
 
+
+
 const links = [
   { href: '/', label: 'Home' },
   { href: '/', label: 'About' },
 ];
 
 export default function Header() {
-
+  const { mode, toggleMode} = React.useContext(ThemeContext);
+  // const textColor = mode === 'dark' ? 'text-gray-300' : 'text-gray-900';
   const [openLogin, setOpenLogin] = React.useState(false);
   return (
-    <header className='sticky h-20 top-0 z-50 bg-white shadow-lg'>
+    <header className={clsx('h-24 top-0 z-50 flex', mode === 'dark' ? 'text-white' : 'text-black' )}>
 
       <LoginForm openLogin={openLogin} setOpenLogin={setOpenLogin}/>
 
       <div className='layout flex pt-2 h-14 content-center justify-between'>
-        <Logo/> 
-        
-        <nav className='mt-2  '>
-          <DesktopNav setOpenLogin={setOpenLogin}/>
-          <Button 
-            variant='light' 
-            className='lg:hidden' 
-            onClick={()=>setOpenLogin(true)}
-          >
-            Login
-          </Button>
+        <Logo withText={true} mode={mode}/> 
+        <nav className='mt-2'>
+          <DesktopNav mode={mode} toggleMode={toggleMode} setOpenLogin={setOpenLogin}/>
           {/* <MobileNavDropDown/> */}
         </nav>
       </div>
