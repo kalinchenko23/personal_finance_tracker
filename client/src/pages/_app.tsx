4e154dc921668e0a2f6ecfaf440407bddeaf78ce
Ruslan Useinov <r.useinov23@gmail.com>
2022-09-13 18:38:00 -0400

    Theme context created

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import ThemeProvider, {ThemeContext} from '@/context/ThemeProvider'
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
@@ -10,7 +11,11 @@ import '@/styles/colors.css';
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp;
