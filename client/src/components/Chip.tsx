51d4c97e25c863efe0805dda478538a30c82d38d
Ruslan Useinov <r.useinov23@gmail.com>
2022-09-14 17:14:31 -0400

    Styles changed

--- client/src/components/Chip.tsx
+++ client/src/components/Chip.tsx
@@ -6,9 +6,9 @@ const Chip = ({text}:{text:string}) => {
     <div className='bg-gray-100 border border-slate-400 px-3 py-1 m-1 rounded-xl text-gray-800
     font-semibold tracking-wider
     transition duration-150 ease-out hover:ease-in
-    hover:bg-gradient-to-tr from-primary-900 via-primary-600 to-primary-500 hover:border-transparent
+    hover:bg-gradient-to-tr from-primary-400 via-primary-600 to-primary-500 hover:border-transparent
     hover:text-white cursor-pointer'>{text}</div>
   )
 }
 
-export default Chip
+export default Chip;
