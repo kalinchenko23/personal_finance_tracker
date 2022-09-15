8a400ff0538e511709fff8f179330b5c8564afd5
Ruslan Useinov <r.useinov23@gmail.com>
2022-09-15 11:22:40 -0400

    basic charts added

---
+++ client/src/components/charts/StackedBar.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import {faker} from '@faker-js/faker'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart - Stacked',
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const fakeDataset = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: 'rgb(255, 99, 132)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: 'rgb(75, 192, 192)',
+    },
+    {
+      label: 'Dataset 3',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: 'rgb(53, 162, 235)',
+    },
+  ],
+};
+
+const StackedBar:React.FC<{
+    width:string, height:string, isFakeData?:boolean
+}> = ({width, height, isFakeData}) => {
+
+  return (
+    <Bar 
+        data={fakeDataset} 
+        width={width} 
+        height={height} 
+        options={options} 
+     />
+  )
+};
+
+export default StackedBar;
+
