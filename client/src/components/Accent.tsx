7c6782f01cc4e6b5c4e63ef5f7db2ca27f7371a0
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-09 21:38:45 -0400

    style: color gradient change

--- client/src/components/Accent.tsx
+++ client/src/components/Accent.tsx
@@ -1,31 +1,27 @@
 import clsx from 'clsx';
 import * as React from 'react';
 
-const Accent:React.FC<{
-  children:any
-  className?:string,
-  green?:boolean | undefined
-  orange?: boolean | undefined
-  red?: boolean | undefined
-}> = ({
-  children,
-  className,
-  green, 
-  orange, 
-  red,
-}) => {
- 
-  const color = green ? 'from-green-400 via-green-600 to-green-500 '
-  : orange ?  'from-orange-400 via-orange-600 to-orange-500 ' 
-  : red ? 'from-red-400 via-red-600 to-red-500 ' 
-  : 'from-primary-400 via-primary-600 to-primary-500 '
+const Accent: React.FC<{
+  children: any;
+  className?: string;
+  green?: boolean | undefined;
+  orange?: boolean | undefined;
+  red?: boolean | undefined;
+}> = ({ children, className, green, orange, red }) => {
+  const color = green
+    ? 'from-green-400 via-green-600 to-green-500 '
+    : orange
+    ? 'from-orange-400 via-orange-600 to-orange-500 '
+    : red
+    ? 'from-red-400 via-red-600 to-red-500 '
+    : 'from-primary-700 via-primary-600 to-primary-400 ';
 
   return (
     <span
       className={clsx(
-        `font-bold text-transparent 
-        bg-gradient-to-tr 
-        bg-clip-text`,
+        `bg-gradient-to-tr bg-clip-text 
+        font-bold 
+        text-transparent`,
         color,
         className
       )}
@@ -33,6 +29,6 @@ const Accent:React.FC<{
       {children}
     </span>
   );
-}
+};
 
 export default Accent;
