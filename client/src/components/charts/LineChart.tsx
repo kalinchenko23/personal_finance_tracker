a4d778895d5a0772574016fe7467b798ac9eed1a
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-13 10:18:59 -0400

    style: line chart styles updated

--- client/src/components/charts/LineChart.tsx
+++ client/src/components/charts/LineChart.tsx
@@ -1,130 +1,162 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
+import type { ChartData, ChartArea, ScriptableContext } from 'chart.js';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  Filler,
 } from 'chart.js';
-
+import { Chart } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Filler,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+  Filler
 );
 
-// import { Chart } from 'react-chartjs-2';
-import { Line } from 'react-chartjs-2';
-import { ThemeContext } from '@/context/ThemeProvider';
-import {faker} from '@faker-js/faker'
-import type { ChartData, ChartArea, ScriptableContext } from 'chart.js';
+const LineChart: React.FC<{
+  width: string;
+  height: string;
+  isFakeData?: boolean;
+}> = ({ width, height, isFakeData }) => {
+  const chartRef = useRef<ChartJS>(null);
+  const [chartData, setChartData] = useState<ChartData<'line'>>({
+    datasets: [],
+  });
 
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'lime',
-    'green',
-    'teal',
-    'blue',
-    'purple',
-];
+  useEffect(() => {
+    const chart = chartRef.current;
 
-// export const data = {
-//     months,
-//     datasets: [
-//       {
-//         label: 'Dataset 1',
-//         data: months.map(() => faker.datatype.number({ min: 1000, max: 20000 })),
-//       },
-//       {
-//         label: 'Dataset 2',
-//         data: months.map(() => faker.datatype.number({ min: 1000, max: 20000 })),
-//       },
-//     ],
-//   };
+    if (!chart) {
+      return;
+    }
 
-
-const data = () => {
-    return {
-      labels: months,
-      datasets: [{
-        // label: "First dataset",
-        data: months.map(() => faker.datatype.number({ min: 1000, max: 20000 })),
-        fill: "start",
-        backgroundColor: ({chart:{ctx}}: ScriptableContext<"line">) => {
-          const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-          gradient.addColorStop(0, "#fde047");
-          gradient.addColorStop(1, "#f97316");
-          return gradient;
-        },
-        borderColor: "transparent"
-      }]
-    };
-};
-
-
-
-const LineChart:React.FC<{
-    width:string, height:string, isFakeData?:boolean
-}> = ({width, height, isFakeData}) => {
-
-    const {mode} = React.useContext(ThemeContext);
-
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins:{
-            legend:{
-                display:false
-            },
-        },
-        elements:{
-            line:{
-                tension:.4,
-                borderWidth:7,
-                borderColor: `${'rgba(250,0,0, 0)'}`,
-                fill:'start',
-                backgroundColor:'transparent'
-            },
-            point:{
-                radius:0,
-                itRadius:1
-            },
-        },
-        scales:{
-            xAxis: {
-                display: false,
-            },
-            yAxis: {
-                display:false
-            },
-            x: {
-                grid: {
-                    color: 'transparent'
-                }
-            }
-        },
+    const chartData = {
+      ...data,
+      datasets: data.datasets.map((dataset) => ({
+        ...dataset,
+        borderColor: 'rgba(255,108,35, 1)',
+        backgroundColor: createGradient(chart.ctx, chart.chartArea),
+      })),
     };
 
-    return (
-        <Line 
-            data={data()} 
-            width={width} 
-            height={height} 
-            options={options} 
-        />
-    )
+    setChartData(chartData);
+  }, []);
+
+  return (
+    <Chart
+      ref={chartRef}
+      type='line'
+      data={chartData}
+      options={options}
+      width={width}
+      height={height}
+    />
+  );
 };
 
 export default LineChart;
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'lime',
+  'green',
+  'teal',
+  'blue',
+  'purple',
+];
+
+const data = {
+  labels: labels.map((month) => month.slice(0, 3)),
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 10000, max: 20000 })),
+    },
+  ],
+};
+
+function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea) {
+  const colorStart = 'rgba(253, 224, 71, .6)';
+  const colorMid = 'rgba(255,108,35, .7)';
+  const colorEnd = 'rgba(255,108,35, 1)';
+
+  const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+  gradient.addColorStop(0, colorStart);
+  gradient.addColorStop(0.5, colorMid);
+  gradient.addColorStop(1, colorEnd);
+
+  return gradient;
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+  elements: {
+    line: {
+      tension: 0.2,
+      borderWidth: 2,
+      fill: 'start',
+    },
+    point: {
+      radius: 0,
+      itRadius: 1,
+    },
+  },
+  scales: {
+    y: {
+      display: true,
+      grid: {
+        color: 'gray',
+      },
+      autoSkip: true,
+      ticks: {
+        // Include a dollar sign in the ticks
+        callback: (value: string | number, index: number, ticks: any) => {
+            let formatter = Intl.NumberFormat('en', {
+                notation: 'compact',
+                compactDisplay: 'short',
+            });
+            return index % 2 === 0 ? '$' + formatter.format(Number(value)) : '';
+        },
+      },
+    },
+    x: {
+      display: true,
+      grid: {
+        color: 'transparent',
+      },
+      autoSkip: true,
+    },
+  },
+};
