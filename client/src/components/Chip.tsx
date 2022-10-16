4c7ed50083b56c8f4e4ad891c163e9b7f2bc5174
Ruslan Useinov <r.useinov23@gmail.com>
2022-10-16 14:22:47 -0400

    fix: mobile card style fix

--- client/src/components/Chip.tsx
+++ client/src/components/Chip.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { ThemeContext } from '@/context/ThemeProvider';
 import clsx from 'clsx';
 
-
-const Chip = ({text, className}:{text:string, className?:string}) => {
+const Chip = ({ text, className }: { text: string; className?: string }) => {
   const { mode } = React.useContext(ThemeContext);
 
-  const chipStyle = mode === 'dark' ? 
-  'bg-gray-100 rounded-md text-gray-800'
-  : 'bg-zinc-800 rounded-md text-gray-50';
+  const chipStyle =
+    mode === 'dark'
+      ? 'bg-gray-100 rounded-md text-gray-800'
+      : 'bg-zinc-800 rounded-md text-gray-50';
 
   return (
-    <div className={clsx(
-      'font-semibold p-1',
-      'transition duration-150 ease-out hover:ease-in',
-      'hover:bg-gradient-to-tr from-primary-700 via-primary-600 to-primary-400 hover:border-transparent',
-      'hover:text-white cursor-pointer inline-block',
-      chipStyle,
-      className
-    )}>
+    <div
+      className={clsx(
+        'p-1 font-semibold',
+        'transition duration-150 ease-out hover:ease-in',
+        'hover:border-transparent hover:bg-gradient-to-tr',
+        'from-primary-700 via-primary-600 to-primary-400',
+        'inline-block cursor-pointer hover:text-white',
+        chipStyle,
+        className
+      )}
+    >
       {text}
     </div>
-  )
-}
+  );
+};
 
 export default Chip;
