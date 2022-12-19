7c03de73ca88b1369ae521758c68f1709bc59825
Maksym Kalinchenko <kalinchenko.max@gmail.com>
2022-12-19 15:56:33 -0500

    /access_token minor issue fix

--- backend/database/sql_service/token_service.py
+++ backend/database/sql_service/token_service.py
@@ -17,7 +17,7 @@ from db_tables import Users,Tokens,base
 #Token specific functions
 
 async def create_access_token(session: AsyncSession, access_token:str,user_id:int):
-    token=Tokens_pydantic(**{"user_id":user_id,"token":access_token,"bank_name":bank_name})
+    token=Tokens_pydantic(**{"user_id":user_id,"token":access_token})
     session.add(Tokens(**token.dict()))
     await session.commit()
 
