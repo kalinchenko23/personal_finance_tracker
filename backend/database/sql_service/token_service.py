f43a620ccb1f9d972c7e90ae9c92db5c59ca85e6
Maksym Kalinchenko <kalinchenko.max@gmail.com>
2022-12-19 19:30:58 -0500

    refactor bank_name assignment, when creating access token

--- backend/database/sql_service/token_service.py
+++ backend/database/sql_service/token_service.py
@@ -16,8 +16,8 @@ from db_tables import Users,Tokens,base
 
 #Token specific functions
 
-async def create_access_token(session: AsyncSession, access_token:str,user_id:int):
-    token=Tokens_pydantic(**{"user_id":user_id,"token":access_token})
+async def create_access_token(session: AsyncSession, access_token:str,user_id:int,bank_name:str):
+    token=Tokens_pydantic(**{"user_id":user_id,"token":access_token,"bank_name":bank_name})
     session.add(Tokens(**token.dict()))
     await session.commit()
 
